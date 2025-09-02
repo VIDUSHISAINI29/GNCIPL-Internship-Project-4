277fc93c07e61b8a24f9b1214e7627487bec0b61
Vidushi Saini <work.vidushi.s@gmail.com>
2025-09-02 18:51:11 +0530

    .

--- backend/src/controllers/auth/loginController.js
+++ backend/src/controllers/auth/loginController.js
@@ -10,7 +10,7 @@ export const loginUser = async(req, res) => {
         if(!user) return res.status(404).json({message: 'User not found'});
         
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!user) return res.status(400).json({message: 'Invalid credentials'});
+        if(!isMatch) return res.status(400).json({message: 'Invalid credentials'});
 
         const token = jwt.sign({
             userId: user._id,
